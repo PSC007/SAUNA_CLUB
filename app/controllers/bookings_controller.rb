bc5c32b1ba683eb10b0c651381b7b0f6d015eb75
Brieuc Labiouse <brieuclabiouse@gmail.com>
2021-02-23 14:26:42 +0100

    save change before going back to master

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,15 @@
 class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new(booking_params)
+    @sauna = Sauna.find(params[:sauna_id])
+    @booking.sauna = @sauna
+    if @booking.save
+      redirect_to
+  end
 
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
- resources :saunas, only: [ :index, :new, :create ]
+ resources :saunas, only: [ :index, :new, :create ] do
+   resources :bookings, only: [:create]
+ end
+
 
   devise_for :users
   root to: "pages#home"
