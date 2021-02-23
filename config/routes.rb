50fb2b70430ddc6ae703686ecfe1022036d71793
Lisbeth Purrucker <lisbeth@cuba-buddy.de>
2021-02-23 12:04:42 +0100

    added sauna routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'saunas/index'
-  get 'saunas/show'
-  get 'saunas/new'
-  get 'saunas/create'
-  get 'saunas/edit'
-  get 'saunas/update'
-  get 'saunas/destroy'
+ resources :saunas, only: [ :new, :create ]
+
   devise_for :users
   root to: "pages#home"
 end
