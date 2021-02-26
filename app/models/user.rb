cfa9aca1180fa4c0ba8ec2348e4d8fb032487977
Marcel Feindt <marcelfeindt@me.com>
2021-02-26 15:47:22 +0100

    changing the card

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :saunas
-  has_many :bookings
+  has_many :saunas, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   has_one_attached :photo
 end
